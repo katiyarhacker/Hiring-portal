64b0622145cf76aee3cca819522cb087ac374230
kk786 <techkhalid19@gmail.com>
2026-02-17 03:41:28 +0530

    added middleware authorisation

--- Back-End/routes/authRoutes.js
+++ Back-End/routes/authRoutes.js
@@ -1,10 +1,21 @@
 import express from "express";
 import { registerUser, loginUser } from "../controllers/authController.js";
+import { protect } from "../middleware/authMiddleware.js";
+
 
 const router = express.Router();
 
 // Register route
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+
+// Test protected route
+router.get("/profile", protect, (req, res) => {
+    res.json({
+        message: "Profile accessed",
+        user: req.user,
+    });
+});
+
 
 export default router;
